c647a8269b2304127e1fea4b0880cc8b03ed1bc0
franb123 <franciscobeistegui@gmail.com>
2016-07-30 16:42:35 -0500

    scroll animation

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 
 
-
+/////// STICKY ///////
 $('.main-nav').sticky({
 	getWidthFrom: '.main-header',
 	responsiveWidth: true
 });
 
+   
+   
+
+//// SCROLL //////////
+$('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
